2af9629e7f87ad9aa4cf419a56ee3bf05206cec4
medi <mehdi.mohammedi@hotmail.fr>
2024-11-14 14:25:57 +0100

    fin

--- assets/carousel1.js
+++ assets/carousel1.js
@@ -1,91 +1,80 @@
-
 const slides = [
-	{
-		"image":"slide1.webp",
-		
-	},
-	{
-		"image":"slide2.webp",
-		
-	},
-    {
-		"image":"slide3.webp",
-		
-	}
-
-]
+  {
+    image: "slide1.webp",
+  },
+  {
+    image: "slide2.webp",
+  },
+  {
+    image: "slide3.webp",
+  },
+];
 
 // Img + Text
 const banner = document.getElementById("carousel1");
 let index = 0;
-console.log(banner)
+console.log(banner);
 
 const slide = document.createElement("img");
 slide.classList.add("banner-img");
 slide.src = `assets/images/${slides[index].image}`;
 banner.appendChild(slide);
 
-
 //Dots
 const dots = document.querySelector(".dots");
 
 for (let i = 0; i < slides.length; i++) {
-	let dot = document.createElement("span");
-	dot.classList.add("dot")
-	dot.classList.add(`dot${i}`)
-	dots.appendChild(dot)
+  let dot = document.createElement("span");
+  dot.classList.add("dot");
+  dot.classList.add(`dot${i}`);
+  dots.appendChild(dot);
 }
 
 let dotSelected = document.querySelector(`.dot${index}`);
 dotSelected.classList.add("dot_selected");
 
 //ARROWS
-const arrowLeft = document.getElementById("angle-left1");
-const arrowRight = document.getElementById("angle-right1");
-
+const arrowLeft = document.querySelector(".fa-angle-left");
+const arrowRight = document.querySelector(".fa-angle-right");
 
 function ArrowDotCLick(index) {
-	dotSelected.classList.remove(`dot_selected`);
-	slide.src = `assets/images/${slides[index].image}`;
-	dotSelected = document.querySelector(`.dot${index}`);
-	dotSelected.classList.add("dot_selected"); 
+  dotSelected.classList.remove(`dot_selected`);
+  slide.src = `assets/images/${slides[index].image}`;
+  dotSelected = document.querySelector(`.dot${index}`);
+  dotSelected.classList.add("dot_selected");
 }
-
-
 
 function arrowClick(direction) {
-	switch (direction) {
-		case "left":
-			index--;
-			if (index < 0) {
-				index = slides.length - 1;
-			}
-			break;
-		case "right":
-			index++;
-			if (index > slides.length - 1) {
-				index = 0;
-			}
-			break;
-		}
-		
-	ArrowDotCLick(index)
-	}
+  switch (direction) {
+    case "left":
+      index--;
+      if (index < 0) {
+        index = slides.length - 1;
+      }
+      break;
+    case "right":
+      index++;
+      if (index > slides.length - 1) {
+        index = 0;
+      }
+      break;
+  }
 
-arrowLeft.addEventListener("click", () => {
-	arrowClick("left");
-});
-arrowRight.addEventListener("click", () => {
-	arrowClick("right");
-});
-
-// Dots click 
-
-for (let i = 0; i < slides.length ; i++) {
-	let dot = document.querySelector(`.dot${i}`)
-	dot.addEventListener("click", () => {
-		ArrowDotCLick(i);
-	});
+  ArrowDotCLick(index);
 }
 
-console.log(index)
+arrowLeft.addEventListener("click", () => {
+  arrowClick("left");
+});
+arrowRight.addEventListener("click", () => {
+  arrowClick("right");
+});
+
+// Dots click
+
+for (let i = 0; i < slides.length; i++) {
+  let dot = document.querySelector(`.dot${i}`);
+  dot.addEventListener("click", () => {
+    ArrowDotCLick(i);
+  });
+}
--- assets/carousel2.js
+++ assets/carousel2.js
@@ -1,91 +1,84 @@
-
 const slides2 = [
-	{
-		"image":"slide4.webp",
-		
-	},
-	{
-		"image":"slide5.webp",
-		
-	},
-    {
-		"image":"slide6.webp",
-		
-	}
+  {
+    image: "slide4.webp",
+  },
+  {
+    image: "slide5.webp",
+  },
+  {
+    image: "slide6.webp",
+  },
+  {
+    image: "slide7.webp",
+  },
+  {
+    image: "slide8.webp",
+  },
+];
 
-]
-
-// Img + Text
 const banner2 = document.getElementById("carousel2");
 let index2 = 0;
-
 
 const slide2 = document.createElement("img");
 slide2.classList.add("banner-img");
 slide2.src = `assets/images/${slides2[index2].image}`;
 banner2.appendChild(slide2);
 
-
-//Dots
+// Dots
 const dots2 = document.getElementById("dots2");
 
-for (let i = 0; i < slides.length; i++) {
-	let dot2 = document.createElement("span");
-	dot2.classList.add("dot")
-	dot2.classList.add(`dot${i}`)
-	dots2.appendChild(dot2)
+for (let i = 0; i < slides2.length; i++) {
+  let dot2 = document.createElement("span");
+  dot2.classList.add("dot2");
+  dot2.classList.add(`dot2${i}`);
+  dots2.appendChild(dot2);
 }
 
-let dotSelected2 = document.querySelector(`.dot${index2}`);
-dotSelected2.classList.add("dot_selected");
+let dotSelected2 = document.querySelector(`.dot2${index2}`);
+dotSelected2.classList.add("dot_selected2");
 
-//ARROWS
+// Arrows
 const arrowLeft2 = document.getElementById("angle-left");
 const arrowRight2 = document.getElementById("angle-right");
 
-
-function ArrowDotCLick(index2) {
-	dotSelected2.classList.remove(`dot_selected`);
-	slide2.src = `assets/images/${slides2[index2].image}`;
-	dotSelected2 = document.querySelector(`.dot${index2}`);
-	dotSelected2.classList.add("dot_selected"); 
+function ArrowDotClick2(index) {
+  dotSelected2.classList.remove(`dot_selected2`);
+  slide2.src = `assets/images/${slides2[index].image}`;
+  dotSelected2 = document.querySelector(`.dot2${index}`);
+  dotSelected2.classList.add("dot_selected2");
 }
 
+function arrowClick2(direction) {
+  switch (direction) {
+    case "left":
+      index2--;
+      if (index2 < 0) {
+        index2 = slides2.length - 1;
+      }
+      break;
+    case "right":
+      index2++;
+      if (index2 > slides2.length - 1) {
+        index2 = 0;
+      }
+      break;
+  }
 
-
-function arrowClick(direction) {
-	switch (direction) {
-		case "left":
-			index2--;
-			if (index2 < 0) {
-				index2= slides2.length - 1;
-			}
-			break;
-		case "right":
-			index2++;
-			if (index2 > slides2.length - 1) {
-				index2 = 0;
-			}
-			break;
-		}
-		
-	ArrowDotCLick(index2)
-	}
+  ArrowDotClick2(index2);
+}
 
 arrowLeft2.addEventListener("click", () => {
-	arrowClick("left");
+  arrowClick2("left");
 });
 arrowRight2.addEventListener("click", () => {
-	arrowClick("right");
+  arrowClick2("right");
 });
 
-// Dots click 
+// Dots click
 
-for (let i = 0; i < slides.length ; i++) {
-	let dot = document.querySelector(`.dot${i}`)
-	dot.addEventListener("click", () => {
-		ArrowDotCLick(i);
-	});
+for (let i = 0; i < slides2.length; i++) {
+  let dot2 = document.querySelector(`.dot2${i}`);
+  dot2.addEventListener("click", () => {
+    ArrowDotClick2(i);
+  });
 }
-
-console.log(`assets/images/${slides2[index2].image}`)
